3a2d48fbe3ac63f9200b15054f494905479d6bd0
Niclas Roßberger <niclas@nidomiro.de>
2021-10-16 21:35:17 +0200

    feat(config-helper): adds first test and corresponding implementation

--- packages/config-helper/src/lib/config-helper.spec.ts
+++ packages/config-helper/src/lib/config-helper.spec.ts
@@ -1,7 +1,18 @@
-import { configHelper } from './config-helper';
+import { configuration } from '@nidomiro/config-helper';
 
 describe('configHelper', () => {
-  it('should work', () => {
-    expect(configHelper()).toEqual('config-helper');
-  });
+
+	it('should add env var to schema on first level', () => {
+
+		const config = configuration({
+			testProp: {
+				default: 'testPropValue'
+			}
+		});
+
+		expect(config.getSchema().properties).toHaveProperty('testProp.env');
+		// @ts-ignore
+		expect(config.getSchema().properties['testProp']['env']).toEqual('TEST_PROP');
+	});
+
 });
--- packages/config-helper/src/lib/config-helper.ts
+++ packages/config-helper/src/lib/config-helper.ts
@@ -1,3 +1,43 @@
-export function configHelper(): string {
-  return 'config-helper';
+import * as convict from 'convict';
+import { constantCase } from 'change-case';
+
+const convict_format_with_validator = require('convict-format-with-validator');
+convict.addFormats(convict_format_with_validator);
+
+
+function addEnv<T>(schema: convict.Schema<T>): convict.Schema<T> {
+
+	function iterate(currentObject: convict.Schema<T>, currentPath: string[]): convict.Schema<T> {
+		const hasDefault = 'default' in currentObject;
+		const hasEnv = 'env' in currentObject;
+		if (hasDefault && !hasEnv) {
+			return {
+				...currentObject,
+				env: constantCase(currentPath.join('_'))
+			};
+		} else if (!hasDefault) {
+			const alteredObjects: convict.Schema<T>[] = Object.entries(currentObject).map(
+				(entry) => {
+					const [key, value] = entry;
+					let newVal = value;
+					if (typeof value === 'object' && value != null) {
+						newVal = iterate(value as convict.Schema<T>, [...currentPath, key]);
+					}
+					return {
+						[key]: newVal
+					} as convict.Schema<T>;
+				}
+			);
+			return Object.assign({}, ...alteredObjects);
+		} else {
+			return currentObject;
+		}
+	}
+
+	return iterate(schema, []);
+}
+
+export function configuration<T>(schema: convict.Schema<T>, opts?: convict.Options): convict.Config<T> {
+
+	return convict(addEnv(schema), opts);
 }
