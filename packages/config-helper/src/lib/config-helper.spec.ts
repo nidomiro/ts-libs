55a3b53c27e18ffe90801c0f0c768c46de73e906
Niclas Roßberger <niclas@nidomiro.de>
2021-10-17 20:25:03 +0200

    style(workspace): format code

--- packages/config-helper/src/lib/config-helper.spec.ts
+++ packages/config-helper/src/lib/config-helper.spec.ts
@@ -1,13 +1,11 @@
 import { createConfig } from './config-helper';
 
 describe('configHelper', () => {
-
 	it('should add env var to schema on first level', () => {
-
 		const config = createConfig({
 			testProp: {
-				default: 'testPropValue'
-			}
+				default: 'testPropValue',
+			},
 		});
 
 		expect(config.getSchema().properties).toHaveProperty('testProp.env');
@@ -17,12 +15,11 @@ describe('configHelper', () => {
 	});
 
 	it('should keep existing env var definitions', () => {
-
 		const config = createConfig({
 			testProp: {
 				default: 'testPropValue',
-				env: 'TEST_ENV_VAR'
-			}
+				env: 'TEST_ENV_VAR',
+			},
 		});
 
 		expect(config.getSchema().properties).toHaveProperty('testProp.env');
@@ -32,7 +29,6 @@ describe('configHelper', () => {
 	});
 
 	it('should add env var definitions to nested config', () => {
-
 		const config = createConfig({
 			group: {
 				testProp: {
@@ -42,9 +38,8 @@ describe('configHelper', () => {
 					testProp: {
 						default: 'testPropValue',
 					},
-				}
-			}
-
+				},
+			},
 		});
 
 		expect(config.getSchema().properties).toHaveProperty('group.properties.testProp.env');
@@ -56,7 +51,8 @@ describe('configHelper', () => {
 		expect(config.getSchema().properties).toHaveProperty('group.properties.innerGroup.properties.testProp.env');
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-expect-error
-		expect(config.getSchema().properties.group.properties.innerGroup.properties.testProp.env).toEqual('GROUP_INNER_GROUP_TEST_PROP');
+		expect(config.getSchema().properties.group.properties.innerGroup.properties.testProp.env).toEqual(
+			'GROUP_INNER_GROUP_TEST_PROP'
+		);
 	});
-
 });
--- packages/config-helper/src/lib/config-helper.ts
+++ packages/config-helper/src/lib/config-helper.ts
@@ -5,30 +5,26 @@ import { constantCase } from 'change-case';
 const CONVICT_FORMAT_WITH_VALIDATOR = require('convict-format-with-validator');
 convict.addFormats(CONVICT_FORMAT_WITH_VALIDATOR);
 
-
 function addEnv<T>(schema: convict.Schema<T>): convict.Schema<T> {
-
 	function iterate(currentObject: convict.Schema<T>, currentPath: string[]): convict.Schema<T> {
 		const hasDefault = 'default' in currentObject;
 		const hasEnv = 'env' in currentObject;
 		if (hasDefault && !hasEnv) {
 			return {
 				...currentObject,
-				env: constantCase(currentPath.join('_'))
+				env: constantCase(currentPath.join('_')),
 			};
 		} else if (!hasDefault) {
-			const alteredObjects: Array<convict.Schema<T>> = Object.entries(currentObject).map(
-				(entry) => {
-					const [key, value] = entry;
-					let newVal = value;
-					if (typeof value === 'object' && value != null) {
-						newVal = iterate(value as convict.Schema<T>, [...currentPath, key]);
-					}
-					return {
-						[key]: newVal
-					} as convict.Schema<T>;
+			const alteredObjects: Array<convict.Schema<T>> = Object.entries(currentObject).map((entry) => {
+				const [key, value] = entry;
+				let newVal = value;
+				if (typeof value === 'object' && value != null) {
+					newVal = iterate(value as convict.Schema<T>, [...currentPath, key]);
 				}
-			);
+				return {
+					[key]: newVal,
+				} as convict.Schema<T>;
+			});
 			return Object.assign({}, ...alteredObjects) as convict.Schema<T>;
 		} else {
 			return currentObject;
@@ -39,6 +35,5 @@ function addEnv<T>(schema: convict.Schema<T>): convict.Schema<T> {
 }
 
 export function createConfig<T>(schema: convict.Schema<T>, opts?: convict.Options): convict.Config<T> {
-
 	return convict(addEnv(schema), opts);
 }
